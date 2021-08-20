dca57fcdc8240f47b795f4410a7118e338854d75
Hoon <dkhj0615@naver.com>
2021-08-20 14:31:12 +0900

    day12 code 전격 업데이트!

---
+++ src/com/care/test/Testmain.java
@@ -0,0 +1,15 @@
+package com.care.test;
+
+import com.care.zzz.*;
+public class Testmain {
+	public static void main(String[] args) {
+		Test t = new Test();
+		t.test();
+		TestClass tc = new TestClass();
+		tc.testClass();
+		Testclass02 tc02 = new Testclass02();
+		tc02.testClass02();
+	
+	}
+
+}
---
+++ src/com/care/zzz/Testclass02.java
@@ -0,0 +1,7 @@
+package com.care.zzz;
+
+public class Testclass02 {
+	public void testClass02() {
+		System.out.println("testclass02 ½ÇÇà");
+	}
+}
